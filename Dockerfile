2c2bedfbcfd7a91bfd2433a16bd11d08c579eefb
Grzegorz Golawski <grzegol@gmail.com>
2023-08-22 12:11:08 +0200

    Use kong version in tag

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,14 @@
-FROM kong/kong-gateway:3.4.0.0-debian@sha256:3fc33cb788c1a1722a51c603115f8625380d4700861f1d20866fcaaa26efe895
+ARG KONG_VERSION
+FROM kong/kong-gateway:${KONG_VERSION}-debian
 
 USER root
 
+SHELL ["/bin/bash", "-o", "pipefail", "-c"]
+# hadolint ignore=DL3008
 RUN apt-get update \
     # Must be separate apt-get invocations because of this bug https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=1039472
-    && apt-get install -y ca-certificates-java \
-    && apt-get install -y openjdk-17-jre-headless \
+    && apt-get install --no-install-recommends -y ca-certificates-java \
+    && apt-get install --no-install-recommends -y openjdk-17-jre-headless \
     && apt-get remove -y openjdk-11-jre-headless \
     && rm -rf /var/lib/apt/lists/* \
     && java -version 2>&1 | grep 'openjdk version "17\.' \
--- build.sh
+++ build.sh
@@ -13,19 +13,19 @@ create_tag() {
     then
         git fetch --tags --force
         current_version_at_head=$(git tag --points-at HEAD)
-        if [[ -z ${current_version_at_head} ]] || [[ ! "${current_version_at_head}" =~ ^v+ ]] || [[ "${push}" == "no" ]];
+        if [[ -z ${current_version_at_head} ]] || [[ ! "${current_version_at_head}" =~ ^b+ ]] || [[ "${push}" == "no" ]];
         then 
             commit_hash=$(git rev-list --tags --topo-order --max-count=1)
             latest_version=""
             if [[ "${commit_hash}" != "" ]]; then            
               latest_version=$(git describe --tags ${commit_hash} 2>/dev/null)
             fi;
-            if [[ ${latest_version} =~ ^v+ ]];
+            if [[ ${latest_version} =~ ^b+ ]];
             then 
                 read a b c <<< $(echo $latest_version|sed 's/\./ /g')
                 version="$a.$b.$((c+1))"
             else
-                version="v1.0.0"
+                version="b1.0.0"
             fi;
 	          echo "version: ${version}"
         else
@@ -39,10 +39,10 @@ create_tag
 if [[ ! -z ${version} ]];
 then
   source project.properties
-  image_version_tag="${owner}/${project}:${version}"
+  image_version_tag="${owner}/${project}:${kong_version}-${version}"
   image_latest_tag="${owner}/${project}:latest"
   echo building ${image_version_tag}
-  docker build --no-cache -t ${image_version_tag} .
+  docker build --no-cache -t ${image_version_tag} --build-arg KONG_VERSION=${kong_version} .
   docker tag ${image_version_tag} ${image_latest_tag}
   if [[ "${push}" == "yes" ]]; then 
     docker push ${image_version_tag}
